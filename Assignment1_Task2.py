ef711c8159fb4d1e160654df00286b64022b0396
Ruthvik3344 <palruthvik3344@gmail.com>
2025-06-17 17:57:18 +0530

    Add files via upload

---
+++ Assignment1_Task2.py
@@ -0,0 +1,4 @@
+a=str(input("Enter the first Name: "))
+b=str(input("Enter the last Name: "))
+c=a+b
+print('Hello! ',c,'! Welcome to the Python Program.')
---
+++ Assignment1_task1.py
@@ -0,0 +1,10 @@
+f1=float(input("Enter the first number: "))
+f2=float(input("Enter the second number: "))
+a=f1+f2
+m=f1*f2
+d=f1/f2
+s=f1-f2
+print("Addition: ",a)
+print("Subtraction: ",s)
+print("Multiplication: ",m)
+print("Division: ",d)
